62550b56d717348049649f06befab1cd71715a21
JLeonn <jsexton11295@gmail.com>
2016-11-14 01:09:22 -0600

    Added Mathagram project

--- C#/Goldilocks/Goldilocks/Program.cs
+++ C#/Goldilocks/Goldilocks/Program.cs
@@ -5,11 +5,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/// <summary>
+/// Inspired by r/dailyprogrammer
+/// https://www.reddit.com/r/dailyprogrammer/comments/5bn0b7/20161107_challenge_291_easy_goldilocks_bear/
+/// </summary>
 namespace Goldilocks
 {
     public class Program
     {
-        private static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.WriteLine(string.Join(",", NextGoodSeat(args[0]).Select(x => x.ToString()).ToArray()));
         }
@@ -17,17 +21,23 @@ namespace Goldilocks
         private static IEnumerable<int> NextGoodSeat(string filePath)
         {
             StreamReader reader = new StreamReader(filePath);
-            int[] goldiProperties = Array.ConvertAll(reader.ReadLine().Split(' '), s => int.Parse(s));
+            int[] goldiProperties = SplitAndParse(reader.ReadLine());
             int seatIndex = 1;
             while (reader.Peek() > -1)
             {
-                int[] chairProperties = Array.ConvertAll(reader.ReadLine().Split(' '), s => int.Parse(s));
+                int[] chairProperties = SplitAndParse(reader.ReadLine());
                 if ((goldiProperties[0] <= chairProperties[0]) && goldiProperties[1] >= chairProperties[1])
                 {
                     yield return seatIndex;
                 }
                 seatIndex++;
             }
+            reader.Close();
+        }
+
+        private static int[] SplitAndParse(string line)
+        {
+            return Array.ConvertAll(line.Split(' '), x => int.Parse(x));
         }
     }
 }
