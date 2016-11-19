519139f247f865a8fcd19d0fcd17c7e0ef54b2c2
JLeonn <jsexton11295@gmail.com>
2016-11-18 20:24:19 -0600

    Updated permutation methods to be Enumerable
    -Fixed bug that caused not enough memory exception
    -Made application much more efficient

--- C#/Mathagrams/Mathagrams/PermutationUtility.cs
+++ C#/Mathagrams/Mathagrams/PermutationUtility.cs
@@ -9,8 +9,6 @@ namespace Mathagrams
     // Contains permutation utilities
     public class PermutationUtility
     {
-        private static List<string> Permutations;
-
         private static void Swap(ref char a, ref char b)
         {
             if (a == b) return;
@@ -20,28 +18,30 @@ namespace Mathagrams
             a ^= b;
         }
 
-        public static List<string> GetPermutations(char[] list)
+        public static IEnumerable<string> GetPermutations(char[] list)
         {
             int x = list.Length - 1;
-            Permutations = new List<string>();
-            GetPermutations(list, 0, x);
-            return Permutations;
+            foreach (string perm in GetPermutations(list, 0, x))
+            {
+                yield return perm;
+            }
         }
 
-        // TODO: Modify function to become Enumerable.
-        // Bug: Crashes application because of too much memory usage.
-        private static void GetPermutations(char[] list, int k, int m)
+        private static IEnumerable<string> GetPermutations(char[] list, int k, int m)
         {
             if (k == m)
             {
-                Permutations.Add(new string(list));
+                yield return new string(list);
             }
             else
             {
                 for (int i = k; i <= m; i++)
                 {
                     Swap(ref list[k], ref list[i]);
-                    GetPermutations(list, k + 1, m);
+                    foreach (string perm in GetPermutations(list, k + 1, m))
+                    {
+                        yield return perm;
+                    }
                     Swap(ref list[k], ref list[i]);
                 }
             } 
--- C#/Mathagrams/Mathagrams/Program.cs
+++ C#/Mathagrams/Mathagrams/Program.cs
@@ -23,8 +23,13 @@ namespace Mathagrams
                 "xxx+xxx+5x3+123=xxx+795",
                 "xxx+xxx+23x+571=xxx+x82",
                 "xxx+xxx+xx7+212=xxx+889",
-                "xxx+xxx+1x6+142=xxx+553"
-                // "xxx+xxx+xxx+x29+821=xxx+xxx+8xx+867" Causes OutOfMemoryException
+                "xxx+xxx+1x6+142=xxx+553",
+                "xxx+xxx+xxx+x29+821=xxx+xxx+8xx+867",
+                "xxx+xxx+xxx+4x1+689=xxx+xxx+x5x+957",
+                "xxx+xxx+xxx+64x+581=xxx+xxx+xx2+623",
+                "xxx+xxx+xxx+x81+759=xxx+xxx+8xx+462",
+                "xxx+xxx+xxx+6x3+299=xxx+xxx+x8x+423",
+                "xxx+xxx+xxx+58x+561=xxx+xxx+xx7+993"
             };
 
         public static void Main(string[] args)
