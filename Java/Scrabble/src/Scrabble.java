091ac533dd9cfa92d0221b42dae5cb5347c912a0
JLeonn <jsexton11295@gmail.com>
2016-12-21 20:24:13 -0600

    Added bonus 3 to Scrabble java project

--- Java/Scrabble/src/Scrabble.java
+++ Java/Scrabble/src/Scrabble.java
@@ -8,29 +8,92 @@ import java.util.stream.Stream;
 /**
  * Inspired by r/dailyprogrammer
  * https://www.reddit.com/r/dailyprogrammer/comments/5go843/20161205_challenge_294_easy_rack_management_1/
+ * All bonuses
  */
 public class Scrabble {
     private static final String DICTIONARY_PATH = "Data//EnglishWords.txt";
+    private static final String HIGHEST_TESTS_PATH = "Data//HighestFunctionTests.txt";
     private static final String LONGEST_TESTS_PATH = "Data//LongestFunctionTests.txt";
     private static final String SCRABBLE_TESTS_PATH = "Data//ScrabbleFunctionTests.txt";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
         loadFile(new File(SCRABBLE_TESTS_PATH))
                 .forEach(x -> {
                     String[] t = x.split(",");
                     System.out.println(scrabble(t[0], t[1]));
                 });
 
+        // TODO: Must be a prettier way to implement this.
         loadFile(new File(LONGEST_TESTS_PATH))
-                .forEach(x -> System.out.println(longest(x)));
+                .forEach(x ->{
+                    try {
+                        System.out.println(longest(x));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        System.exit(1);
+                    }
+                });
 
+        // TODO: Must be a prettier way to implement this.
+        loadFile(new File(HIGHEST_TESTS_PATH))
+                .forEach(x ->{
+                    try {
+                        System.out.println(highest(x));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        System.exit(1);
+                    }
+                });
     }
 
-    public static String highest(String tiles){
-        return ""; // temporary
+    public static int getScore(String word){
+        int score = 0;
+        for (char c : word.toCharArray()) {
+            switch (c){
+                case '?':
+                    score += 0;
+                    break;
+                case 'a': case 'e': case 'i': case 'l': case 'n': case 'o': case 'r': case 's': case 't': case 'u':
+                    score += 1;
+                    break;
+                case 'd': case 'g':
+                    score += 2;
+                    break;
+                case 'b': case 'c': case 'm': case 'p':
+                    score += 3;
+                    break;
+                case 'f': case 'h': case 'v':  case 'w': case 'y':
+                    score += 4;
+                    break;
+                case 'k':
+                    score += 5;
+                    break;
+                case 'j': case 'x':
+                    score += 8;
+                    break;
+                case 'q': case 'z':
+                    score += 10;
+                    break;
+            }
+        }
+        return score;
     }
 
-    public static String longest(String tiles) {
+    // TODO: Combine highest and longest function by passing the KeyExtractor.
+    public static String highest(String tiles) throws IOException{
+        Optional<String> result = loadFile(new File(DICTIONARY_PATH))
+                .filter(x -> scrabble(tiles, x))
+                .max(Comparator.comparing(Scrabble::getScore));
+
+        return result.isPresent() ? result.get() : null;
+    }
+
+    public static Stream<String> loadFile(File file) throws IOException{
+        return Files.lines(file.toPath());
+    }
+
+    // TODO: Combine highest and longest function by passing the KeyExtractor.
+    public static String longest(String tiles) throws IOException {
         Optional<String> result = loadFile(new File(DICTIONARY_PATH))
                 .filter(x -> scrabble(tiles, x))
                 .max(Comparator.comparingInt(String::length));
@@ -38,6 +101,7 @@ public class Scrabble {
         return result.isPresent() ? result.get() : null;
     }
 
+    // TODO: Find more efficient way of implementation.
     public static boolean scrabble(String tiles, String word){
         for (char c : word.toCharArray()){
             if (tiles.indexOf(c) != -1){
@@ -49,13 +113,5 @@ public class Scrabble {
             }
         }
         return true;
-    }
-
-    public static Stream<String> loadFile(File file) {
-        try{
-            return Files.lines(file.toPath());
-        } catch (IOException e) {
-            return null;
-        }
     }
 }
