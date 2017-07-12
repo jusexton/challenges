06158721579ab56b4675c03b3e8560f4beb9422e
JSextonn <jsexton11295@gmail.com>
2017-07-12 01:29:44 -0500

    Added RootMeanSquare Project

---
+++ RosettaCode/C#/RootMeanSquare/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RootMeanSquare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(RootMeanSquare(Enumerable.Range(1, 10)));
+        }
+
+        private static double RootMeanSquare(IEnumerable<int> values)
+        {
+            var count = values.Count();
+            if (count == 0)
+            {
+                throw new InvalidOperationException("Empty Collection");
+            }
+
+            double squaredSum = values.Sum(value => value * value);
+            return Math.Sqrt(squaredSum / count);
+        }
+    }
+}
