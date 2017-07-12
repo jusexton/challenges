60d96be98e9455aabd4c49024bf54c4819aafea5
JSextonn <jsexton11295@gmail.com>
2017-07-11 19:34:09 -0500

    Added ArithmeticMean Project

---
+++ RosettaCode/C#/ArithmeticMean/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ArithmeticMean
+{
+    class Program
+    {
+        private static readonly int[] testNumbers = { 90, 100, 95, 20, 40 };
+
+        private static void Main(string[] args)
+        {
+            Console.WriteLine($"Mean Of {string.Join(", ", testNumbers)} Is {Mean(testNumbers)}");
+        }
+
+        private static double Mean(IEnumerable<int> values) => values.Average();
+    }
+}
