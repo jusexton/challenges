695874eb1ce2533f5ba52e8547192521dd696db2
JSextonn <jsexton11295@gmail.com>
2017-07-12 00:42:44 -0500

    Added Median Project

---
+++ RosettaCode/C#/Median/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Median
+{
+    class Program
+    {
+        private static readonly int[] testNumbers = { 1, 5, 3, 6, 4, 2 };
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Median(testNumbers));
+            Console.WriteLine(Median(testNumbers.Take(5)));
+        }
+
+        private static double Median(IEnumerable<int> values)
+        {
+            var count = values.Count();
+            if (count == 0)
+            {
+                throw new InvalidOperationException("Empty Collection");
+            }
+
+            var sortedValues = values.OrderBy(i => i);
+            var mid = count / 2;
+            if (count % 2 == 0)
+            {
+                return (double)(sortedValues.ElementAt(mid) + sortedValues.ElementAt(mid + 1)) / 2;
+            }
+            return sortedValues.ElementAt(mid);
+        }
+    }
+}
