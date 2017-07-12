11daa27b0be17722c05f0a8d23af5d783a8ba201
JSextonn <jsexton11295@gmail.com>
2017-07-12 01:13:26 -0500

    Added Mode Project

---
+++ RosettaCode/C#/Mode/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Mode
+{
+    class Program
+    {
+        private static readonly int[] testNumbers = { 1, 5, 3, 7, 1, 4, 7, 5, 5 };
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Mode(testNumbers));
+            Console.WriteLine(Mode(testNumbers.Take(7)));
+        }
+
+        private static int Mode(IEnumerable<int> values)
+        {
+            return values.GroupBy(i => i)
+                .OrderByDescending(group => group.Count())
+                .Select(group => group.Key)
+                .First();
+        }
+    }
+}
