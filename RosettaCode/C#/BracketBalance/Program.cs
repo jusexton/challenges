6b6f3694cef845660a289b692df533392e6c52d7
JSextonn <jsexton11295@gmail.com>
2017-07-12 01:59:38 -0500

    Added BarcketBalance Project

---
+++ RosettaCode/C#/BracketBalance/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace BracketBalance
+{
+    class Program
+    {
+        private static readonly string[] testText =
+        {
+         "",
+         "[]",
+         "[][]",
+         "[[][]]",
+         "][",
+         "][][",
+         "[]][[]"
+        };
+
+        static void Main(string[] args)
+        {
+            foreach (var text in testText)
+            {
+                Console.WriteLine($"{text} Valid: {BracketsBalanced(text)}");
+            }
+        }
+
+        private static bool BracketsBalanced(string text, char open = '[', char closed = ']')
+        {
+            var level = 0;
+            foreach (var character in text)
+            {
+                if (character == closed)
+                {
+                    if (level == 0)
+                    {
+                        return false;
+                    }
+                    level--;
+                }
+                else if (character == open)
+                {
+                    level++;
+                }
+            }
+            return level == 0;
+        }
+    }
+}
