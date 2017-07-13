74e9adfe28575c585f0cdf54a6f0e5071af95985
Justin Sexton <jsexton11295@gmail.com>
2017-07-13 09:12:54 -0500

    Added BinarySearch Project

---
+++ RosettaCode/C#/BinarySearch/BinarySearch/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BinarySearch
+{
+    class Program
+    {
+        private static readonly int[] testNumbers = { 1, 2, 3, 4, 6, 7, 8, 10 }; 
+
+        static void Main(string[] args)
+        {
+            const int key = 8;
+            Console.WriteLine(Array.BinarySearch(testNumbers, key));
+            Console.WriteLine(BinarySearch(testNumbers, key));
+        }
+
+        private static int BinarySearch(IReadOnlyCollection<int> array, int key)
+        {
+            var low = 0;
+            var high = array.Count;
+
+            while (low <= high)
+            {
+                var mid = (high + low) / 2;
+
+                if (array.ElementAt(mid) > key)
+                {
+                    high = mid - 1;
+                }
+                else if (array.ElementAt(mid) < key)
+                {
+                    low = mid + 1;
+                }
+                else
+                {
+                    return mid;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
