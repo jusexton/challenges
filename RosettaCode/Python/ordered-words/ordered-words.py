9eb78074ea51903c9bea10e235f91ac6f306806c
JSextonn <jsexton11295@gmail.com>
2017-08-07 02:46:37 -0500

    Added ordered-words project

---
+++ RosettaCode/Python/ordered-words/ordered-words.py
@@ -0,0 +1,14 @@
+def main():
+    with open('words.txt') as file:
+        words = [word.strip() for word in file.readlines()]
+
+    ordered_words = [word for word in words if is_ordered(word)]
+    largest_length = len(max(ordered_words, key=len))
+    largest_ordered_words = [word for word in ordered_words if len(word) == largest_length]
+    print(largest_ordered_words, sep='\n')
+
+def is_ordered(word):
+    return ''.join(sorted(word)) == word
+
+if __name__ == '__main__':
+    main()
