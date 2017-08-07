f421bd9dc9ceb0b32a68d1ffce57a049271eb510
JSextonn <jsexton11295@gmail.com>
2017-08-07 03:17:53 -0500

    Added OrderedWords project

---
+++ RosettaCode/C#/OrderedWords/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace OrderedWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var orderedWords = File.ReadAllLines("words.txt").Where(IsOrdered);
+            var largestWordLength = orderedWords.Max(word => word.Length);
+            var result = string.Join(", ", orderedWords.Where(word => word.Length == largestWordLength));
+            Console.WriteLine(result);
+        }
+
+        private static bool IsOrdered(string word)
+        {
+            return String.Concat(word.OrderBy(c => c)) == word;
+        }
+    }
+}
