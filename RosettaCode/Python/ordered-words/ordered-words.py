b57e86f86bb7ec15762b2092e48a06b7f94b88ed
JSextonn <jsexton11295@gmail.com>
2017-08-21 04:05:37 -0500

    Refactored code in an attempt to increase code readability.

--- RosettaCode/Python/ordered-words/ordered-words.py
+++ RosettaCode/Python/ordered-words/ordered-words.py
@@ -2,13 +2,17 @@ def main():
     with open('words.txt') as file:
         words = [word.strip() for word in file.readlines()]
 
-    ordered_words = [word for word in words if is_ordered(word)]
+    ordered_words = list(
+        filter(lambda x: is_ordered(x), words))
     largest_length = len(max(ordered_words, key=len))
-    largest_ordered_words = [word for word in ordered_words if len(word) == largest_length]
+    largest_ordered_words = list(
+        filter(lambda x: len(x) == largest_length, ordered_words))
     print(', '.join(largest_ordered_words))
+
 
 def is_ordered(word):
     return ''.join(sorted(word)) == word
+
 
 if __name__ == '__main__':
     main()
