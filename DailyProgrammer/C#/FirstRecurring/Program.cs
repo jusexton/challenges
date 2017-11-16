7cf334d6c3598ef25bde0ddde500d79940e80f14
JSextonn <jsexton11295@gmail.com>
2017-11-16 13:55:41 -0600

    Added FirstRecurring Project

---
+++ DailyProgrammer/C#/FirstRecurring/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FirstRecurring
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var testInput = new[]
+            {
+                "ABCDEBC",
+                "IKEUNFUVFV",
+                "PXLJOUDJVZGQHLBHGXIW",
+                "*l1J?)yn%R[}9~1\"=k7]9;0[$"
+            };
+
+            foreach (var test in testInput)
+            {
+                Console.WriteLine($"Input: {test}, Result: {FirstRecurringCharacter(test)}");
+            }
+        }
+
+        private static char? FirstRecurringCharacter(string input)
+        {
+            var characters = input.ToCharArray();
+            var visitedCharacters = new List<char>();
+
+            foreach (var character in characters)
+            {
+                if (visitedCharacters.Contains(character))
+                {
+                    return character;
+                }
+                visitedCharacters.Add(character);
+            }
+            return null;
+        }
+    }
+}
