f6fb62e847a74847f13fb27b51df5f3cf7025e31
JSextonn <jsexton11295@gmail.com>
2018-02-02 21:33:11 -0600

    Added CharacterFrequency project.

---
+++ RosettaCode/C#/CharacterFrequency/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace CharacterFrequency
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var characterFrequencyMap = File.ReadAllText("data.txt")
+                .GroupBy(c => c)
+                .OrderBy(c => c.Key)
+                .ToDictionary(group => group.Key, group => group.Count());
+
+            foreach (var entry in characterFrequencyMap)
+            {
+                Console.WriteLine($"{entry.Key} - {entry.Value}");
+            }
+        }
+    }
+}
