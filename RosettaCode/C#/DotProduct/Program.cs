c077a74945aedf22c5e990d869554c385a68eff6
JSextonn <jsexton11295@gmail.com>
2018-02-07 22:08:23 -0600

    Added DotProduct project.

---
+++ RosettaCode/C#/DotProduct/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace DotProduct
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var vectorOne = new double[] { 5, 4 };
+            var vectorTwo = new double[] { 5, 3 };
+            Console.WriteLine(CalculateDotProduct(vectorOne, vectorTwo));
+        }
+
+        private static double CalculateDotProduct(double[] vectorOne, double[] vectorTwo)
+        {
+            if (vectorOne.Length != vectorTwo.Length)
+            {
+                throw new InvalidOperationException("vectors must be in same dimension.");
+            }
+
+            return vectorOne.Zip(vectorTwo, (vOne, vTwo) => vOne * vTwo)
+                .Aggregate((product, next) => next + product);
+        }
+    }
+}
