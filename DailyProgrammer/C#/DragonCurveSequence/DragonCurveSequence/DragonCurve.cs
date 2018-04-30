d9b2415db9b1e9605b9f3dd0fed1d096ac0b1bb9
JSextonn <jsexton11295@gmail.com>
2018-04-30 18:47:59 -0500

    Added DragonCurve C# project

---
+++ DailyProgrammer/C#/DragonCurveSequence/DragonCurveSequence/DragonCurve.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace DragonCurveSequence
+{
+	public static class DragonCurve
+	{
+		public static IEnumerable<string> GetSequence()
+		{
+			var result = "1";
+			while (true)
+			{
+				yield return result;
+				result = $"{result}{result}0";
+			}
+		}
+	}
+}
---
+++ DailyProgrammer/C#/DragonCurveSequence/DragonCurveSequence/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace DragonCurveSequence
+{
+	public static class Program
+	{
+		private static void Main() =>
+			DragonCurve.GetSequence()
+				.Take(8)
+				.ToList()
+				.ForEach(Console.WriteLine);
+	}
+}
