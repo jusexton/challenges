21de3946c70d036086489d87d7d28741fe372373
JSextonn <jsexton11295@gmail.com>
2018-07-10 05:44:27 -0500

    Added KnuthUpArrow project.

---
+++ DailyProgrammer/C#/KnuthUpArrow/KnuthUpArrow/KnuthUpArrowCalculator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Numerics;
+using System.Text.RegularExpressions;
+
+namespace KnuthUpArrow
+{
+	// TODO: Allow calculator to be able to handle much larger numbers.
+	public class KnuthUpArrowCalculator
+	{
+		// TODO: Allow negative number to be passed in expression.
+		private const string ParseString = @"^(?<a>([0-9]|[1-9][0-9]))(?<arrows>\^+)(?<b>([0-9]|[1-9][0-9]))$";
+
+		public static BigInteger Calculate(string expression)
+		{
+			var matchResult = new Regex(ParseString).Match(expression);
+
+			if (!matchResult.Success)
+			{
+				throw new ArgumentException("expression not formatted correctly", nameof(expression));
+			}
+
+			var a = Convert.ToInt32(matchResult.Groups["a"].Value);
+			var b = Convert.ToInt32(matchResult.Groups["b"].Value);
+			var arrowCount = matchResult.Groups["arrows"].Value.Length;
+			return Calculate(a, b, arrowCount);
+		}
+
+		public static int Calculate(int a, int b, int arrowCount)
+		{
+			if (arrowCount == 1)
+			{
+				return Convert.ToInt32(Math.Pow(a, b));
+			}
+
+			if (arrowCount >= 1 && b == 0)
+			{
+				return 1;
+			}
+
+			// ReSharper disable once TailRecursiveCall
+			return Calculate(a, Calculate(a, b - 1, arrowCount), arrowCount - 1);
+		}
+	}
+}
---
+++ DailyProgrammer/C#/KnuthUpArrow/KnuthUpArrow/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace KnuthUpArrow
+{
+	public static class Program
+	{
+		private static readonly string[] TestExpressions =
+		{
+			"2^4",
+			"2^^4",
+			// "5^^^^5"
+			"-1^^^3"
+		};
+
+		private static void Main() =>
+			TestExpressions.Select(KnuthUpArrowCalculator.Calculate)
+				.Select(number => number.ToString())
+				.ToList()
+				.ForEach(Console.WriteLine);
+	}
+}
