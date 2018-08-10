1b93d26fffe1e32473674a53d0e35c209c2bc537
JSextonn <jsexton11295@gmail.com>
2018-08-10 02:26:58 -0500

    Removed kotlin longest-word and added C# LongestWord project.

---
+++ CoderByte/C#/LongestWord/LongestWord/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LongestWord
+{
+	public static class Program
+	{
+		private static void Main()
+		{
+			Console.WriteLine(Factorial(Convert.ToInt32(Console.ReadLine())));
+		}
+
+		public static int Factorial(int num)
+		{
+			if (num >= 1)
+			{
+				return num * Factorial(num - 1);
+			}
+
+			return 1;
+		}
+	}
+}
