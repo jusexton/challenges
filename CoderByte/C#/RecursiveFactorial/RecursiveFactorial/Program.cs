aba78fed7f1163da1b6c7d20e0f34935bc29a801
JSextonn <jsexton11295@gmail.com>
2018-08-10 02:35:08 -0500

    Added RecursiveFactorial project.

---
+++ CoderByte/C#/RecursiveFactorial/RecursiveFactorial/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace RecursiveFactorial
+{
+	public static class Program
+	{
+		private static void Main()
+		{
+			Console.WriteLine(Factorial(Convert.ToInt32(Console.ReadLine())));
+		}
+
+		private static int Factorial(int num)
+		{
+			if (num >= 1)
+			{
+				return num * Factorial(num - 1);
+			}
+
+			return 1;
+		}
+	}
+}
