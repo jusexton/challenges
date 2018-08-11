0fbcee4400501f7de9ebf8f78aea6c234214c1ce
JSextonn <jsexton11295@gmail.com>
2018-08-10 23:25:17 -0500

    Added SimpleAdding project.

---
+++ CoderByte/C#/SimpleAdding/SimpleAdding/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace SimpleAdding
+{
+	public static class Program
+	{
+		private static void Main()
+		{
+			Console.WriteLine(SimpleAdding(4));
+		}
+
+		private static int SimpleAdding(int num) => Enumerable.Range(1, num).Sum();
+	}
+}
