051bc85403f00ea6a9135253f61bacc169bd333f
JSextonn <jsexton11295@gmail.com>
2018-08-10 23:24:59 -0500

    Added ReverseString project.

---
+++ CoderByte/C#/ReverseString/ReverseString/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace ReverseString
+{
+	public static class Program
+	{
+		private static void Main()
+		{
+			Console.WriteLine(ReverseString("hello"));
+		}
+
+		private static string ReverseString(string str) => string.Join("", str.Reverse());
+
+//		private static string ReverseString(string str)
+//		{
+//			var stringBuilder = new StringBuilder();
+//			for (var i = str.Length - 1; i >= 0; i--)
+//			{
+//				stringBuilder.Append(str.ElementAt(i));
+//			}
+//
+//			return stringBuilder.ToString();
+//		}
+	}
+}
