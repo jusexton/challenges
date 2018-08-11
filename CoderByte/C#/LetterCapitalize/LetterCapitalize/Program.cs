cd27004766ab80848e4a681f58a4831583dd5d29
JSextonn <jsexton11295@gmail.com>
2018-08-10 23:24:16 -0500

    Added LetterCapitalize project.

---
+++ CoderByte/C#/LetterCapitalize/LetterCapitalize/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace LetterCapitalize
+{
+	public static class Program
+	{
+		private static void Main()
+		{
+			Console.WriteLine(LetterCapitalize("hello world"));
+		}
+
+		private static string LetterCapitalize(string str) =>
+			Regex.Replace(str, @"(^\w)|(\s\w)", m => m.Value.ToUpper());
+	}
+}
