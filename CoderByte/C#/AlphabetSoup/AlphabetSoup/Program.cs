bd73ce57b4a0cba9f68deff73e99a0ea139b3b86
JSextonn <jsexton11295@gmail.com>
2018-08-12 02:15:09 -0500

    Added AlphabetSoup project.

---
+++ CoderByte/C#/AlphabetSoup/AlphabetSoup/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace AlphabetSoup
+{
+	public static class Program
+	{
+		private static void Main()
+		{
+			Console.WriteLine(AlphabetSoup("hello"));
+		}
+
+		private static string AlphabetSoup(string str) => string.Join("", str.OrderBy(c => c));
+	}
+}
