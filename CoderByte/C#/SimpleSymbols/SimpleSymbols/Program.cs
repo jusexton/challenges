2e0607e0b7b7977221b5446349d63597ec4b7b05
JSextonn <jsexton11295@gmail.com>
2018-08-12 02:16:05 -0500

    Added SimpleSymbols project.

---
+++ CoderByte/C#/SimpleSymbols/SimpleSymbols/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace SimpleSymbols
+{
+	public static class Program
+	{
+		private static void Main()
+		{
+			Console.WriteLine(SimpleSymbols("f++d+"));
+		}
+
+		private static bool SimpleSymbols(string str) =>
+			Regex.Matches(str, @"\+[a-z]\+").Count == str.Count(char.IsLetter);
+	}
+}
