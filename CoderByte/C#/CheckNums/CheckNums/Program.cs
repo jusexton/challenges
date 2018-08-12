c5df5980409d72b96c3227020b08fbd0ef78f7ed
JSextonn <jsexton11295@gmail.com>
2018-08-12 02:15:31 -0500

    Added CheckNums project.

---
+++ CoderByte/C#/CheckNums/CheckNums/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CheckNums
+{
+	public static class Program
+	{
+		private static void Main()
+		{
+			Console.WriteLine(CheckNums(6, 6));
+		}
+
+		private static string CheckNums(int numOne, int numTwo)
+		{
+			if (numTwo > numOne)
+			{
+				return "true";
+			}
+
+			return numTwo < numOne ? "false" : "-1";
+		}
+	}
+}
