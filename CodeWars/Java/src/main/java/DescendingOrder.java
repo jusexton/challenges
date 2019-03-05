865be8aa5cd0e35d1d3f964527689c687a43a6de
Justin Sexton <jsexton@mac-0ywjg5j.r1-core.r1.aig.net>
2019-03-05 11:52:48 -0600

    Reformatted comments

--- CodeWars/Java/src/main/java/DescendingOrder.java
+++ CodeWars/Java/src/main/java/DescendingOrder.java
@@ -1,10 +1,10 @@
 public final class DescendingOrder {
-//    public static int sortDesc(final int num) {
-//        return Integer.parseInt(String.valueOf(num).chars()
-//                .mapToObj(Character::toString)
-//                .sorted(Comparator.reverseOrder())
-//                .collect(Collectors.joining("")));
-//    }
+    // public static int sortDesc(final int num) {
+    //     return Integer.parseInt(String.valueOf(num).chars()
+    //             .mapToObj(Character::toString)
+    //             .sorted(Comparator.reverseOrder())
+    //             .collect(Collectors.joining("")));
+    // }
 
     public static int sortDesc(final int num) {
         String reversedNumber = new StringBuilder(String.valueOf(num)).reverse().toString();
