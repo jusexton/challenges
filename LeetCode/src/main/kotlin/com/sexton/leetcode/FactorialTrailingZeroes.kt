4e184b62bb24dd2662116754edbb3ec3054ed781
Justin Sexton <jsexton11295@gmail.com>
2019-08-12 19:18:58 -0500

    Remoevd TODO comment. Reformatted.

--- LeetCode/src/main/kotlin/com/sexton/leetcode/FactorialTrailingZeroes.kt
+++ LeetCode/src/main/kotlin/com/sexton/leetcode/FactorialTrailingZeroes.kt
@@ -3,16 +3,15 @@ package com.sexton.leetcode
 object FactorialTrailingZeroes {
     // fun trailingZeroes(n: Int): Int = n.factorial().toString().takeLastWhile { it == '0' }.count()
 
-    // TODO: Needs refactoring
-//    fun trailingZeroes(n: Int): Int {
-//        var count = 0
-//        var i = 5
-//        while (n.div(i) >= 1) {
-//            count += n.div(i)
-//            i *= 5
-//        }
-//        return count
-//    }
+    //    fun trailingZeroes(n: Int): Int {
+    //        var count = 0
+    //        var i = 5
+    //        while (n.div(i) >= 1) {
+    //            count += n.div(i)
+    //            i *= 5
+    //        }
+    //        return count
+    //    }
 
     fun trailingZeroes(n: Int): Int = if (n == 0) 0 else n / 5 + trailingZeroes(n / 5)
 }
