0a9bb2c13e5247c9b7908374d4b5fd8191214c2a
Justin Sexton <jsexton11295@gmail.com>
2019-08-12 19:12:20 -0500

    Removed Power Solution from project. Fixed bug in Isomorphic that displayed strings to be isomorphic when they were not.

--- LeetCode/src/main/kotlin/com/sexton/leetcode/Isomorphic.kt
+++ LeetCode/src/main/kotlin/com/sexton/leetcode/Isomorphic.kt
@@ -3,25 +3,38 @@ package com.sexton.leetcode
 import java.util.concurrent.ConcurrentHashMap
 
 object Isomorphic {
+    /**
+     * Determines if [strOne] and [strTwo] are isomorphic.
+     */
     fun isIsomorphic(strOne: String, strTwo: String): Boolean {
+        // Not possible for the strings to be isomorphic if they do not even have the same length.
         if (strOne.length != strTwo.length) {
             return false
         }
 
         val seen = ConcurrentHashMap<Char, Char>()
         return strOne.zip(strTwo).all {
+            // If the first character in the pair has not been seen before.
             if (!seen.containsKey(it.first)) {
+
+                // And the second character in the pair has been seen. Then the strings are not isomorphic.
                 if (seen.containsValue(it.second)) {
                     return false
                 } else {
+                    // Update to show we have seen the character and the character is was seen along side with.
+                    // This should never override data, only add
                     seen[it.first] = it.second
                 }
             } else {
+                // The first character in the pair has been seen.
+                // Therefore the value it was seen with must match the value it is now being seen with again.
+                // Otherwise, the strings are not isomorphic.
                 if (seen[it.first] != it.second) {
                     return false
                 }
             }
-            return true
+
+            true
         }
     }
 }
