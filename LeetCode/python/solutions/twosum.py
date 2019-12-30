89aae3281f79a4c711df0c7cd21fb86be8d0ee37
Justin Sexton <jsexton11295@gmail.com>
2019-12-29 22:47:40 -0600

    Added Python TwoSum solution

---
+++ LeetCode/python/solutions/twosum.py
@@ -0,0 +1,11 @@
+def two_sum(numbers: list[int], target: int) -> list[int]:
+    compliment_map = dict()
+    for index in range(0, len(numbers)):
+        number = numbers[index]
+        compliment = target - number
+        if compliment in compliment_map:
+            return [compliment_map[compliment], index]
+
+        compliment_map[number] = index
+
+    raise ValueError('Given numbers does not contain the specified target')
