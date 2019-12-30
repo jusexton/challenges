18efd63f066530bfe2938c6ddf03b6ba159a8256
Justin Sexton <jsexton11295@gmail.com>
2019-12-30 01:49:43 -0600

    Optimized Kotlin TwoSum solution

--- LeetCode/jvm/src/main/kotlin/com/sexton/leetcode/TwoSum.kt
+++ LeetCode/jvm/src/main/kotlin/com/sexton/leetcode/TwoSum.kt
@@ -4,34 +4,15 @@ object TwoSum {
     fun twoSum(numbers: IntArray, target: Int): IntArray {
         require(numbers.size >= 2) { "Given inputs do not have a solution" }
 
-        val result = intArrayOf(0, 0)
-        val indicesMap = mutableMapOf<Int, Int>()
-        for (k in numbers.indices) {
-            if (!indicesMap.containsKey(numbers[k])) {
-                indicesMap[numbers[k]] = k
-            } else if (numbers[k] * 2 == target) {
-                result[0] = indicesMap[numbers[k]] ?: throw NullPointerException("Something terribly wrong happened")
-                result[1] = k
-                return result
+        val complementMap = mutableMapOf<Int, Int>()
+        for (index in numbers.indices) {
+            val number = numbers[index]
+            val complement = target - number
+            if (complementMap.containsKey(complement)) {
+                return intArrayOf(complementMap[complement] ?: throw IllegalStateException("Wont happen"), index)
             }
-        }
 
-        var left = 0
-        var right = numbers.size - 1
-        val sortedNumbers = numbers.sorted()
-        while (left < right) {
-            val sum = sortedNumbers[left] + sortedNumbers[right]
-            when {
-                sum > target -> right--
-                sum < target -> left++
-                sum == target -> {
-                    result[0] = indicesMap[sortedNumbers[left]]
-                        ?: throw NullPointerException("Something terribly wrong happened")
-                    result[1] = indicesMap[sortedNumbers[right]]
-                        ?: throw NullPointerException("Something terribly wrong happened")
-                    return result.sortedArray()
-                }
-            }
+            complementMap[number] = index
         }
 
         throw IllegalArgumentException("Given inputs do not have a solution")
